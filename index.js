ea7826455cc9a784daf86065ea0496a33ca3f8fe
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-03-02 15:11:50 -0500

    Button Animation + keydown events

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let drums = document.querySelectorAll('.drum')
 
 drums.forEach((drum) => {
@@ -6,53 +5,71 @@ drums.forEach((drum) => {
        
         let buttonInnerHTML = this.innerHTML
 
-        switch (buttonInnerHTML) {
-            case 'w':
-                let tom1 = new Audio('./sounds/tom-1.mp3')
-                 tom1.play()
-                break;
+        makeSound(buttonInnerHTML)
 
-            case 'a':
-                let tom2 = new Audio('./sounds/tom-2.mp3')
-                tom2.play()
-                break;
-
-            case 's':
-                let tom3 = new Audio('./sounds/tom-3.mp3')
-                 tom3.play()
-                 break;
-                
-            case 'd': 
-                let tom4 = new Audio('./sounds/tom-4.mp3')
-                 tom4.play()
-                 break;
-            
-            
-             case 'j': 
-                 let crash = new Audio('./sounds/crash.mp3')
-                      crash.play()
-                      break;
-
-             case 'k': 
-                    let kick = new Audio('./sounds/kick-bass.mp3')
-                        kick.play()
-                        break;
-            
-            
-            
-              case 'l': 
-                     let snare = new Audio('./sounds/snare.mp3')
-                         snare.play()
-                         break;       
-            
-                default: console.log(buttonInnerHTML)
-                
-        }
-
-        // let audio = new Audio('./sounds/tom-1.mp3')
-        // audio.play()
+       buttonAnimation(buttonInnerHTML)
 
     })
 })
 
+document.addEventListener('keydown', (event) => {
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
 
+function makeSound(key){
+    
+
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('./sounds/tom-1.mp3')
+             tom1.play()
+            break;
+
+        case 'a':
+            let tom2 = new Audio('./sounds/tom-2.mp3')
+            tom2.play()
+            break;
+
+        case 's':
+            let tom3 = new Audio('./sounds/tom-3.mp3')
+             tom3.play()
+             break;
+            
+        case 'd': 
+            let tom4 = new Audio('./sounds/tom-4.mp3')
+             tom4.play()
+             break;
+        
+        
+         case 'j': 
+             let crash = new Audio('./sounds/crash.mp3')
+                  crash.play()
+                  break;
+
+         case 'k': 
+                let kick = new Audio('./sounds/kick-bass.mp3')
+                    kick.play()
+                    break;
+        
+        
+        
+          case 'l': 
+                 let snare = new Audio('./sounds/snare.mp3')
+                     snare.play()
+                     break;       
+        
+            default: console.log(buttonInnerHTML)
+}}
+
+function buttonAnimation(currentKey) {
+
+   let activeButton = document.querySelector("." + currentKey)
+   activeButton.classList.add('pressed')
+
+   setTimeout(() => {
+       activeButton.classList.remove('pressed')
+   }, 250)
+
+}
+ 
